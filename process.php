03f0ca4000ec9ff8b1d93361193617da43dfebf8
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-10-21 19:08:57 -0400

    fixing form

--- process.php
+++ process.php
@@ -2,32 +2,32 @@
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$message = $_POST["message"];
-	 
-	$EmailTo = "francescoperrozzi3@gmail.com"; 
+
+	$EmailTo = "francescoperrozzi3@gmail.com";
 	$Subject = "Portfolio CV/Resume";
-	 
+
 	// prepare email body text
 	
 	$Body .= "Name: ";
 	$Body .= $name;
-	$Body .= "\n";
-	 
+	$Body .= "\n"; 
+
 	$Body .= "Email: ";
 	$Body .= $email;
 	$Body .= "\n";
-	 
+
 	$Body .= "Message: ";
 	$Body .= $message;
 	$Body .= "\n";
-	 
+
 	
 	// send email
 	$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-	 
+
 	// redirect to success page
 	if ($success){
-	   echo "success";
+echo "success";
 	}else{
-	    echo "invalid";
+echo "invalid";
 	} 
 ?>
